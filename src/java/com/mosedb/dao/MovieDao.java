e886d6023a1e21e961bb886f885933fd36e95fe4
lasse <lybeck.lasse@gmail.com>
2013-04-05 13:46:20 +0300

    Refactored dao structure and added functionality to moviedao.

--- src/java/com/mosedb/dao/MovieDao.java
+++ src/java/com/mosedb/dao/MovieDao.java
@@ -4,7 +4,6 @@
  */
 package com.mosedb.dao;
 
-import com.mosedb.business.UserService;
 import com.mosedb.models.User;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -17,25 +16,48 @@ import java.sql.SQLException;
  */
 public class MovieDao extends AbstractDao {
 
+    /**
+     * Adds a movie entry to the database.
+     *
+     * @param user The owner of the movie.
+     * @param seen Status of the movie, seen or not.
+     * @return The movieid of the movie.
+     * @throws SQLException
+     */
     public int addMovie(User user, boolean seen) throws SQLException {
         String sql = "insert into mosedb.movie (owner,seen) values (?,?)";
         executeUpdate(sql, user.getUsername(), seen);
         return getMovieId(user);
     }
-    
-    public static void main(String[] args) throws SQLException {
-        User lasse = new UserService().getUser("lasse", "salasana");
-        new MovieDao().addMovie(lasse, 1989, true);
-    }
 
+    /**
+     * Adds a movie entry to the database.
+     *
+     * @param user The owner of the movie.
+     * @param movieyear Year the movie was released.
+     * @param seen Status of the movie, seen or not.
+     * @return The movieid of the movie.
+     * @throws SQLException
+     */
     public int addMovie(User user, int movieyear, boolean seen) throws SQLException {
         String sql = "insert into mosedb.movie (owner,movieyear,seen) values (?,?,?)";
         executeUpdate(sql, user.getUsername(), movieyear, seen);
         return getMovieId(user);
     }
-    
-    public void removeMovie(int movieid){
-        
+
+    public boolean removeMovie(int movieid) throws SQLException {
+        String sql = "delete from mosedb.movie where movieid=?";
+        return executeUpdate(sql, movieid);
+    }
+
+    public boolean updateMovieSeen(int movieid, boolean seen) throws SQLException {
+        String sql = "update mosedb.movie set (seen)=(?) where movieid=?";
+        return executeUpdate(sql, seen, movieid);
+    }
+
+    public boolean updateMovieYear(int movieid, int newyear) throws SQLException {
+        String sql = "update mosedb.movie set (movieyear)=(?) where movieid=?";
+        return executeUpdate(sql, newyear, movieid);
     }
 
     /**
