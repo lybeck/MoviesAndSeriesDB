9e10baa7cde47081aa6c829004338d6b1aae885e
llybeck <llybeck@dhp-34.cs.helsinki.fi>
2013-04-22 13:55:47 +0300

    Refactored search page.

--- src/java/com/mosedb/servlets/SearchServlet.java
+++ src/java/com/mosedb/servlets/SearchServlet.java
@@ -24,28 +24,19 @@ import javax.servlet.http.HttpSession;
  */
 public class SearchServlet extends MosedbServlet {
 
+    private static final String SEARCH_FIELD_NAME = "search_field";
+    private static final String SEARCH_FIELD_DEFAULT = "Search";
+    private static final String DROP_BOX_NAME = "drop_box";
     private static final String NAME_SEARCH = "Name";
     private static final String GENRE_SEARCH = "Genre";
     private static final String MEDIAFORMAT_SEARCH = "Media format";
-    private static final String MOVIE_OR_SERIES_SEARCH = "check1";
+    private static final String MOVIE_OR_SERIES_SEARCH = "movie_series_radio";
     private static final String MOVIE_SEARCH = "movie";
     private static final String SERIES_SEARCH = "series";
-    private static final String SEEN_RADIO = "seenRadio";
+    private static final String SEEN_RADIO = "seen_radio";
     private static final String SEEN_CHECKED = "seen";
-    private static final String NOT_SEEN_CHECKED = "notSeen";
+    private static final String NOT_SEEN_CHECKED = "not_seen";
     private static final String BOTH_SEEN_CHECKED = "both";
-
-    private static String searchFieldDefault(int i) {
-        return "Search #" + i;
-    }
-
-    private static String searchFieldName(int i) {
-        return "txt" + i;
-    }
-
-    private static String dropMenuName(int i) {
-        return "select" + i;
-    }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -60,8 +51,8 @@ public class SearchServlet extends MosedbServlet {
             MovieService movieService = new MovieService();
 
 
-            String search1 = request.getParameter(searchFieldName(1));
-            String drop1 = request.getParameter(dropMenuName(1));
+            String searchField = request.getParameter(SEARCH_FIELD_NAME);
+            String dropBox = request.getParameter(DROP_BOX_NAME);
             List<Movie> movieList;
 
             String searchType = request.getParameter(MOVIE_OR_SERIES_SEARCH);
@@ -77,16 +68,16 @@ public class SearchServlet extends MosedbServlet {
 
             if (searchType.equals(MOVIE_SEARCH)) {
 
-                if (search1 == null || search1.isEmpty() || search1.equalsIgnoreCase(searchFieldDefault(1))
-                        || drop1 == null) {
+                if (searchField == null || searchField.isEmpty() || searchField.equals(SEARCH_FIELD_DEFAULT)
+                        || dropBox == null) {
                     movieList = movieService.getMovies(user, seenParameter);
                 } else {
-                    if (drop1.equals(NAME_SEARCH)) {
-                        movieList = movieService.getByName(user, search1, seenParameter);
-                    } else if (drop1.equals(GENRE_SEARCH)) {
-                        movieList = movieService.getByGenre(user, search1, seenParameter);
-                    } else if (drop1.equals(MEDIAFORMAT_SEARCH)) {
-                        movieList = movieService.getByMediaFormat(user, search1, seenParameter);
+                    if (dropBox.equals(NAME_SEARCH)) {
+                        movieList = movieService.getByName(user, searchField, seenParameter);
+                    } else if (dropBox.equals(GENRE_SEARCH)) {
+                        movieList = movieService.getByGenre(user, searchField, seenParameter);
+                    } else if (dropBox.equals(MEDIAFORMAT_SEARCH)) {
+                        movieList = movieService.getByMediaFormat(user, searchField, seenParameter);
                     } else {
                         movieList = movieService.getMovies(user, seenParameter);
                     }
